20484f10362afaa294a22e0b455371892338432a
thapa07lakshya@gmail.com <thapa07lakshya@gmail.com>
2023-11-06 17:38:46 +0000

    Created suite, booking, double and single; Edited menu and Form1

--- client_code/double/__init__.py
+++ client_code/double/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import menuTemplate
+from ._anvil_designer import doubleTemplate
 from anvil import *
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
@@ -7,20 +7,12 @@ import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
 
-class menu(menuTemplate):
+class double(doubleTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
 
-  def button_1_click(self, **event_args):
-    open_form('single')
-
-  def button_2_click(self, **event_args):
-    open_form('double')
-
-  def button_3_click(self, **event_args):
-    open_form('suite')
-    
-  
+  def link_1_click(self, **event_args):
+    open_form('menu')
